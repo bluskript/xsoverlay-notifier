3e04a4dfbd48d8039e8044d1fa7224a4fce841eb
Bluskript <bluskript@gmail.com>
2023-01-01 17:15:30 -0500

    include public certificate in release

--- .github/scripts/build.ps1
+++ .github/scripts/build.ps1
@@ -2,6 +2,7 @@ $bytes = [Convert]::FromBase64String($env:SIGN_CERT)
 [Io.File]::WriteAllBytes('./sign_cert.pfx', $bytes)
 $SIGN_CERT_PASSWORD_SECURE = ConvertTo-SecureString -String $env:SIGN_CERT_PASSWORD -AsPlainText -Force
 Import-PfxCertificate -Password $SIGN_CERT_PASSWORD_SECURE -FilePath ./sign_cert.pfx -Cert Cert:\CurrentUser\TrustedPublisher
+Export-Certificate -Cert (Get-ChildItem Cert:\CurrentUser\TrustedPublisher -CodeSigningCert)[0] -FilePath code_signing.crt
 Copy-Item target/release/xsoverlay_notifier.exe ./windows-packaging
 Set-Location windows-packaging
 & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\MakeAppx.exe" pack /d . /p ../out/XSOverlayNotifier.msix /nv /o
