c12fb5f1938ac5bc013d5ac0f7d8bf6ca04649e5
mia <bluskript@gmail.com>
2024-02-23 20:43:17 +0000

    skip certificate system by just passing -AllowUnsigned

--- install.ps1
+++ install.ps1
@@ -1,9 +1,6 @@
 #Requires -RunAsAdministrator
-$Cert = New-TemporaryFile
 $Installer = New-TemporaryFile
-Invoke-WebRequest -Uri https://github.com/bluskript/xsoverlay-notifier/releases/download/latest/code_signing.crt -OutFile $Cert
 Invoke-WebRequest -Uri https://github.com/bluskript/xsoverlay-notifier/releases/download/latest/XSOverlayNotifier.msix -OutFile $Installer
-Import-Certificate -FilePath $Cert -Cert Cert:\LocalMachine\TrustedPeople
 # Import-Certificate -FilePath $Cert -Cert Cert:\CurrentUser\Root
-Add-AppPackage -Path $Installer
+Add-AppPackage -AllowUnsigned -Path $Installer
 Start-Process "shell:AppsFolder\$((Get-StartApps | Where-Object {$_.Name -eq 'XSOverlay Notifier'}).'AppID')"
